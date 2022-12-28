8c69bd31e0910c1dc300301d20d6455044875343
Robert4361 <robert4361@gmail.com>
2022-12-28 15:18:11 +0100

    Add letter guessing

--- game.rb
+++ game.rb
@@ -9,5 +9,33 @@ class Game
     @word = File.readlines('words.txt').map(&:strip).filter { |word| word.length >= 5 }.sample
     @number_of_guesses = 10
     @lines = '_' * word.length
+    @guessed_letters = ''
+  end
+
+  def guess
+    letter = guess_letter
+
+    @number_of_guesses -= 1
+    @guessed_letters += letter
+
+    return 'wrong_letter' unless @word.include? letter
+
+    swap_guessed_lines(letter)
+  end
+
+  private
+
+  def guess_letter
+    puts 'Write your guess'
+    letter = 'xx'
+    letter = gets.chomp.downcase until letter.length == 1 &&
+                                       letter.between?('a', 'z') &&
+                                       !@guessed_letters.include?(letter)
+    letter
+  end
+
+  def swap_guessed_lines(letter)
+    indexes = @word.chars.each_with_index.select { |char, _index| char == letter }.map { |array| array[1] }
+    indexes.each { |index| @lines[index] = letter }
   end
 end
